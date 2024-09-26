1d1613c0b594316742c3b7a4f68a1924e71fa656
Chirag Saroj <chiragsaroj1804@gmail.com>
2024-09-26 18:52:42 +0530

    socket io integration

--- src/index.js
+++ src/index.js
@@ -16,5 +16,34 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    const { Server } = require("socket.io");
+
+    let io = new Server(strapi.server.httpServer, {
+      cors: { // cors setup
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true,
+      },
+    });
+
+    // Handle socket connection
+    io.on('connection', (socket) => {
+      // console.log(`User connected on Socket: ${socket.id}`);
+
+      socket.on('send-message', (message) => {
+        // Add 360ms delay
+        setTimeout(()=>{
+          console.log(`Message received: ${message}`);
+          socket.emit('receive-message', message)
+        }, 360);
+      });
+
+      // Handle socket disconnection
+      socket.on('disconnect', () => {
+        console.log(`Socket disconnected: ${socket.id}`);
+      });
+    });
+  },
 };
